249677a51fdf0bbde58142eda42439d0d0b238e4
Vidiic <jordividal1997@gmail.com>
2024-01-29 19:05:58 +0100

    router layout & pages

--- src/app/presentation/layouts/dashboard/dashboard.component.ts
+++ src/app/presentation/layouts/dashboard/dashboard.component.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-    selector: 'app-dashboard',
-    standalone: true,
-    imports: [
-        CommonModule,
-    ],
-    templateUrl: './dashboard.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './dashboard.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardComponent { }
+export class DashboardComponent {}
