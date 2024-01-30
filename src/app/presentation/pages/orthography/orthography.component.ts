d0df7ffe556f9fea3b979cd843b74f15e2308faf
Vidiic <jordividal1997@gmail.com>
2024-01-30 22:54:53 +0100

    typingLoader componente

--- src/app/presentation/pages/orthography/orthography.component.ts
+++ src/app/presentation/pages/orthography/orthography.component.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ChatMessageComponent, MyMessageComponent } from '@chatBubbles/index';
+import { TypingLoaderComponent } from 'app/presentation/components/typing-loader/typing-loader.component';
 
 @Component({
   selector: 'app-orthography',
   standalone: true,
-  imports: [CommonModule, ChatMessageComponent, MyMessageComponent],
+  imports: [
+    CommonModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+  ],
   templateUrl: './orthography.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
