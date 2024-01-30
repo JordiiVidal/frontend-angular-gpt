753576732939b87e0f2265ff2525ad47019ade26
Vidiic <jordividal1997@gmail.com>
2024-01-30 22:28:04 +0100

    chat components

--- src/app/presentation/pages/orthography/orthography.component.ts
+++ src/app/presentation/pages/orthography/orthography.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChatMessageComponent } from '../../components/chat-bubbles/chat-message/chat-message.component';
+import { MyMessageComponent } from '../../components/chat-bubbles/my-message/my-message.component';
 
 @Component({
   selector: 'app-orthography',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ChatMessageComponent, MyMessageComponent],
   templateUrl: './orthography.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
